f8758afcc29b3ac77c64ffc56fef88f426a8bae3
root <root@c1181.gconex.com>
2025-07-27 00:14:26 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'infourbi',
+  webDir: 'dist'
+};
+
+export default config;
