5f1c637c600b864bf535bd9669fa94d166266df0
juliosincable <juliosincable@gmail.com>
2025-08-03 16:22:15 +0000

    feat: configuracion inicial de hosting y .gitignore

--- src/service/firebase.ts
+++ src/service/firebase.ts
@@ -1,21 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC9jP9Zkl6mAoH7K48JaANnftG0Q1mIqcE",
-  authDomain: "infourbi-b04de.firebaseapp.com",
-  projectId: "infourbi-b04de",
-  storageBucket: "infourbi-b04de.firebasestorage.app",
-  messagingSenderId: "467109582483",
-  appId: "1:467109582483:web:e911ca7ccab32b85831f66",
-  measurementId: "G-Q3BMVG87XL"
+  apiKey: "AIzaSyDYXFPtlAVzUPuDkREe9sc934Nlc_xIKyc",
+  authDomain: "infourbi-8df9c.firebaseapp.com",
+  projectId: "infourbi-8df9c",
+  storageBucket: "infourbi-8df9c.firebasestorage.app",
+  messagingSenderId: "335432407522",
+  appId: "1:335432407522:web:8b0d021300ee6dfa6341bb",
+  measurementId: "G-V2M81C2R0F"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
